264788fae0f8d6e233943b4b8f3f2f795255770d
gpetrini <gpetrinidasilveira@gmail.com>
2025-07-15 09:33:41 -0300

    [INTERNAL] Post summer school

--- Chaotic_Model/fun_chaos.cpp
+++ Chaotic_Model/fun_chaos.cpp
@@ -3,6 +3,11 @@
 MODELBEGIN
 
 // Your code goes here
+EQUATION("X")
+v[0] = VL("X",1);
+v[1] = V("m");
+v[2] = v[1]*v[0]*(1-v[0]);
+RESULT(v[2])
 
 MODELEND
 void close_sim(void) {}
--- Industry_SummerSchool/fun_industry.cpp
+++ Industry_SummerSchool/fun_industry.cpp
@@ -5,5 +5,16 @@ MODELBEGIN
 
 // Your code goes here
 
+EQUATION("a")
+// Comment
+v[0] = VL("a", 1);
+v[1] = V("eta");
+v[2] = V("beta1");
+v[3] = V("beta2");
+v[4] = beta(v[2], v[3]);
+v[5] = v[0] * (1 + v[1] * v[4]);
+RESULT( v[5] )
+
+
 MODELEND
 void close_sim(void) {}
